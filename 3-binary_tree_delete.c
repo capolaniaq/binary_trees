57b4d02341fdebd82b83298d8f2f7c4fa8756aed
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-20 20:24:24 -0500

    add betty style 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - function that delete binary tree
+ * @tree: pointer to root node
+ *
+ * Return: void
+ */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
@@ -7,7 +14,7 @@ void binary_tree_delete(binary_tree_t *tree)
 
 	if (tree->left != NULL)
 		binary_tree_delete(tree->left);
-	
+
 	if (tree->right != NULL)
 		binary_tree_delete(tree->right);
 
