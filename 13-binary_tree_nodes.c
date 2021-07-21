864a1b2d5272945e5d554c5a6850fe9f84ed9a5a
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-21 17:12:20 -0500

    add betty style to 13-binary_trre_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_nodes - function that count a nodes of a binary tree
+ * @tree: pointer to root of binary tree
+ *
+ * Return: size_t that is the number of nodes
+ */
+
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t nodes = 0;
@@ -15,5 +22,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->right != NULL)
 		nodes = binary_tree_nodes(tree->right) + nodes;
 
-	return(nodes);
+	return (nodes);
 }
