105d8ef90c7728d67125c4cf57794d104b4a0350
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-20 20:44:15 -0500

    add betty style 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_inorder - Funtion that print binary tree
+ * in order traversal
+ * @tree: pointer to root binary tree
+ * @func: pointer to function for print values
+ *
+ * Return: void
+ */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
@@ -12,5 +20,4 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 
 	if (tree->right != NULL)
 		binary_tree_inorder(tree->right, (*func));
-	return;
 }
