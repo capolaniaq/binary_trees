c5af937bb66867f306839f4db00cc8af0428215a
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-20 20:27:08 -0500

    add betty style 4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_leaf - function that check if node is leaf
+ * @node: node to check
+ *
+ * Return: 1 if node is leaf or 0 if not
+ */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
