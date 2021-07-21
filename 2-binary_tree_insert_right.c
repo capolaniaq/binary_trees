097ab83e4fa71ce38f25fc09298e724776cde4e7
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-21 12:42:37 -0500

    fixing the parent== NULL)

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,13 +13,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 	binary_tree_t *tmp;
 
+	if (parent == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = value;
-	if (parent == NULL)
-		return (NULL);
 
 	if (parent->right == NULL)
 	{
