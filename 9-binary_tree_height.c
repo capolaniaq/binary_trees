0ef657ada613022c77b32d133ce718ecf2629709
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-21 13:53:18 -0500

    fixing the tree== NULL case

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,6 +12,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_left = 0;
 	size_t height_right = 0;
 
+	if (tree == NULL)
+		return (0);
+
 	if (tree->left != NULL)
 	{
 		height_left = binary_tree_height(tree->left) + 1;
