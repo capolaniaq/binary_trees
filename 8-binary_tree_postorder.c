b14818df05134b33bdcfb00c105295807e55e65d
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-20 20:47:30 -0500

    add betty style 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_postorder - Funtion that print the binary tree
+ * in post order traversal
+ * @tree: pointer to root from binary tree
+ * @func: pointer to function for print
+ *
+ * Return: void
+ */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
@@ -13,6 +21,4 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 		binary_tree_postorder(tree->right, (*func));
 
 	(*func)(tree->n);
-
-	return;
 }
