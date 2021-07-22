eee1f575b22084e6b810f414af9b071ca15ab4e8
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-21 23:29:42 -0500

    perfect variable

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -29,9 +29,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		height_left = binary_tree_height_perfect(tree->left);
 	if (tree->right != NULL)
 		height_right = binary_tree_height_perfect(tree->right);
-	if ((height_right != height_left) || perfect != 1)
+	if ((height_right != height_left))
 		return (0);
-	return (1);
+	return (perfect);
 
 }
 
