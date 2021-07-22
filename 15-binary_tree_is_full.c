f3acc98d267d5b7d1c673a37993ece30d8f888ad
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-21 21:08:03 -0500

    15-binary_tree_is_full.c

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - function that find if the node is full
+ * @tree: pointer to node check
+ *
+ * Return: int 1 in case success and 0 in case failure
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int full = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if ((tree->left != NULL && tree->right != NULL)
+	|| (tree->left == NULL && tree->right == NULL))
+	{
+		full = 1;
+	}
+	else
+	{
+		return (0);
+	}
+
+	if (tree->left != NULL)
+		full = binary_tree_is_full(tree->left);
+	if (tree->right != NULL)
+		full = binary_tree_is_full(tree->right);
+	return (full);
+}
